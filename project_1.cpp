7c5623a733690954476c9d3e0273d037fbaea27b
Richard "Dylan" McGee <sleepyanorak@gmail.com>
2017-10-06 21:19:30 -0700

    Update project_1.cpp

--- project_1.cpp
+++ project_1.cpp
@@ -1,34 +1,34 @@
 #include <iostream>
 #include <string>
 #include <map>
-int main(){
+
+void board_values():
 std::string board =
-  "  ---------\n"
-  "    |  |   \n"
-  "  ---------\n"
-  "    |  |   \n"
-  "  ---------\n"
-  "    |  |   \n"
-  "  ---------\n";
-
-
-
+  "  -----------\n"
+  "   1 | 2 | 3  \n"
+  "  -----------\n"
+  "   4 | 5 | 6  \n"
+  "  -----------\n"
+  "   7 | 8 | 9  \n"
+  "  -----------\n";
+std::cout << "here is what the gameboard is like with the numbers corresponding to the spaces you can choose"<<endl;
 std::cout << board << std::endl;
 
 
-
+int main(){
+  
 // initialize map with all 0
 std::map<int,bool> mymap;
 for (int x = 0; x < 9; x++){
   mymap[x] = 0;
 }
-
+// make a random integer to put in test "ship"
 int randy = rand() % 10;
 mymap[randy] = 1;
 
 bool checker = true;
 
-
+//while loop until target is hit (maybe in the future change to for loop with limited tries)
 std::cout << "try to hit the target"<<std::endl;
 while (checker){
   int locator;
@@ -40,9 +40,13 @@ while (checker){
   }
   else{
     std::cout << "miss :/"<<std::endl;
+    //show the updated game board
+    if locator == 1;
+      
   }
 
 }
 std::cout << "Yay you beat the game!!!!!!!!!!!!!!"<<std::endl;
 return 0;
 }
+
