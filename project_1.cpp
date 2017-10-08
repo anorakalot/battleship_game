5b5146d0d6e2b43dbdf3ba8f3f4cc17fcc50b267
Richard "Dylan" McGee <sleepyanorak@gmail.com>
2017-10-08 04:32:34 -0700

    Add files via upload

--- project_1.cpp
+++ project_1.cpp
@@ -19,9 +19,9 @@ std::string value_board =
   "   24 | 25 | 26 | 27 | 28 | 29 | 30 | 31 \n"
   "  ---------------------------------------\n";
 std::cout << std::endl <<std::endl;
-std::cout << "Here is what the gameboard is the numbers correspond to the spaces you can choose"<<std::endl<<std::endl;
-std::cout << "During the game you will get B for hitting the bomb and P for gaining a point"<<std::endl<<std::endl;
-std::cout << "try to get as many spaces filled without hitting a bomb"<<std::endl<<std::endl;
+std::cout << "Here is the gameboard. The numbers correspond to the spaces you can choose"<<std::endl<<std::endl;
+std::cout << "During the game you will get S for hitting enemy ships and M for missed shots"<<std::endl<<std::endl;
+std::cout << "Hit the enemy ships in the turns provided or you lose"<<std::endl<<std::endl;
 std::cout << value_board << std::endl;
 }
 
@@ -33,7 +33,7 @@ std::string value_board =
   "   8  | 9  | 10 | 11 | 12 | 13 | 14 | 15 \n"
   "  ---------------------------------------\n"
   "   16 | 17 | 18 | 19 | 20 | 21 | 22 | 23 \n"
-  "  ---------------------------------------\n";
+  "  ---------------------------------------\n"
   "   24 | 25 | 26 | 27 | 28 | 29 | 30 | 31 \n"
   "  ---------------------------------------\n";
 std::cout << "               key of values"<<std::endl;
@@ -53,6 +53,20 @@ std::cout << value_board << std::endl;
 
 
 int main(){
+//need placeholder string for cin
+std::cout << std::endl<<std::endl<<std::endl<<std::endl<<std::endl<<std::endl<<std::endl<<std::endl<<std::endl<<std::endl<<std::endl<<std::endl<<std::endl<<std::endl
+<<std::endl<<std::endl<<std::endl<<std::endl<<std::endl<<std::endl<<std::endl<<std::endl<<std::endl<<std::endl<<std::endl<<std::endl<<std::endl<<std::endl<<std::endl
+<<std::endl<<std::endl<<std::endl<<std::endl<<std::endl<<std::endl<<std::endl<<std::endl<<std::endl<<std::endl<<std::endl<<std::endl<<std::endl<<std::endl<<std::endl
+<<std::endl<<std::endl<<std::endl<<std::endl<<std::endl<<std::endl<<std::endl<<std::endl<<std::endl<<std::endl<<std::endl<<std::endl<<std::endl<<std::endl<<std::endl;
+
+std::string name;
+std::cout <<"HELLO GENERAL WE NEED YOUR HELP MORE THAN EVER"<<std::endl;
+std::cout <<"WE ARE SURROUNDED BY ENEMY SHIPS AND WE NEED YOU TO FIND AND DESTROY THEM USING SONAR TECHNOLOGY!!!"<<std::endl;
+std::cout <<"IF YOU DONT ELIMINATE ALL THE ENEMY SHIPS DURING THE TIME LIMIT WE WILL BE OVERRUN"<<std::endl;
+std::cout << "GOOD LUCK GENERAL"<<std::endl;\
+std::cout << "ENTER YOUR NAME: ";
+std::cin >> name;
+std::cout << std::endl;
 init_board_values();
 // initialize check list with all 0
 // this is whats going to be used to check if the user input hits the target
@@ -61,44 +75,49 @@ for (int x = 0; x < 32; x++){
   checklist[x] = 0;
 }
 
-// make 2 random integers to put in test "bomb" then put into checklist
+//make a map for printing a gameboard with updated values
+std::map<int,std::string> myboard;
+for (int x=0; x < 32;x++ ){
+  myboard[x]='-';
+}
+
+// make 2 random integers to put into checklist
+//these are the enemy "ships"
 srand(time(0));
 int randy_1 = (rand() % 16);
 int randy_2 = (rand() % 32+16);
 checklist[randy_1] = 1;
 checklist[randy_2] = 1;
 
-
-//make a map for printing a gameboard with updated values
-std::map<int,std::string> myboard;
-for (int x=0; x < 32;x++ ){
-  myboard[x]='-';
-}
-//declare variable thats going to be used to control the game loop
-bool loop_controller = true;
 //declare variable thats going to control if people want to contrinue playing
 char play_again;
 // declare points variable
 int points = 0;
 
 //main game loop until win or loss conditions are met
-while (loop_controller){
+//while (loop_controller){
+std::cout << "YOU HAVE 10 TRIES TO TRACK DOWN AND ELEIMINATE ENEMY SHIPS "<<std::endl;
+std::cout << std::endl;
+for(int i = 0; i <10;i++ ){
   int locator;
-  std::cout << "Insert a key value:  ";
+  std::cout << "LOCATION VALUE:  ";
   std::cin >> locator;
   //check if locator is to high
   if (locator>31){
-    std::cout << "Value is too High"<<std::endl;
+    std::cout << "VALUE IS TOO HIGH GENERAL"<<std::endl;
     continue;
+  }
+  else{
+    NULL;
   }
   //check if locator is not an int (do later)
 
 
 
-
+// tell if player hit ship and check win condition
   if (checklist[locator]==1){
-    std::cout << "oh no you hit one of the two bombs  :\\"<<std::endl<<std::endl;
-    myboard[locator] = "B";
+    std::cout << "HIT!"<<std::endl<<std::endl;
+    myboard[locator] = "S";
     std::cout << "-------------------------------------------------------" << std::endl;
     std::cout << myboard[0] << "   |   " << myboard[1] << "   |   "<< myboard[2]<< "  |  " << myboard[3] << "   |   " << myboard[4] << "   |   "<< myboard[5]<< "  |  " << myboard[6] << "   |   " << myboard[7]<<std::endl;
     std::cout << "-------------------------------------------------------" << std::endl;
@@ -108,16 +127,23 @@ while (loop_controller){
     std::cout << "-------------------------------------------------------" << std::endl;
     std::cout << myboard[24] << "   |   " << myboard[25] << "   |   "<< myboard[26]<< "  |  " << myboard[27] << "   |   " << myboard[28] << "   |   "<< myboard[29]<< "  |  " << myboard[30] << "   |   " << myboard[31]<<std::endl;
     std::cout << "-------------------------------------------------------" << std::endl;
-    std::cout << "Heres the final point total: "<<points<<std::endl;
-    loop_controller = false;
-  }
+    //std::cout << "Heres the final point total: "<<points<<std::endl;
+    if (myboard[randy_1]=="S" && myboard[randy_2]=="S"){
+      std::cout << "YOU WIN"<<std::endl;
+      break;
+      //loop_controller = false;
+    }
+    else{
+      std::cout << "ONLY ONE SHIP REMAINS GENERAL "<<name<<std::endl;
+    }
+  }//end of if player hit ship
   else{
-    if (myboard[locator] == "P"){
+    if (myboard[locator] == "M"){
       std::cout << "NO REPEAT VALUES"<<std::endl;
       continue;//checks if locator value has been used before
     }
-    std::cout << "Plus one point!"<<std::endl<<std::endl;
-    myboard[locator] = "P";
+    std::cout << "MISS!"<<std::endl<<std::endl;
+    myboard[locator] = "M";
     std::cout << "-------------------------------------------------------" << std::endl;
     std::cout << myboard[0] << "   |   " << myboard[1] << "   |   "<< myboard[2]<< "  |  " << myboard[3] << "   |   " << myboard[4] << "   |   "<< myboard[5]<< "  |  " << myboard[6] << "   |   " << myboard[7]<<std::endl;
     std::cout << "-------------------------------------------------------" << std::endl;
@@ -128,19 +154,20 @@ while (loop_controller){
     std::cout << myboard[24] << "   |   " << myboard[25] << "   |   "<< myboard[26]<< "  |  " << myboard[27] << "   |   " << myboard[28] << "   |   "<< myboard[29]<< "  |  " << myboard[30] << "   |   " << myboard[31]<<std::endl;
     std::cout << "-------------------------------------------------------" << std::endl;
     std::cout << std::endl<<std::endl;
-    points += 1;
-    std::cout << "Point total: " << points <<std::endl;
-    if (points == 28){
-      std::cout << "You win Awesome Job :)))))))!"<<std::endl;
-      loop_controller = false;
+    //points += 1;
+    //std::cout << "Point total: " << points <<std::endl;
+    //sonar trigger for vertical enemy ship
+    if ((checklist[locator+8]==1) || (checklist[locator-8]==1)){
+      std::cout << "SONAR DETECTED ENEMY SHIP" << std::endl;
+      board_values();
     }
-    else if (checklist[locator+8]==1 || checklist[locator-8]==1){
-      std::cout << "vertical warning" << std::endl;
-    }
-    else if (checklist[locator+1] == 1 || checklist[locator-1]==1){
-      std::cout << "horizontal warning"<<std::endl;
+    //sonar trigger for horizontal enemy ship
+    else if ((checklist[locator+1] == 1) || (checklist[locator-1]==1)){
+      std::cout << "SONAR DETECTED ENEMY SHIP"<<std::endl;
+      board_values();
     }
     else{
+      std::cout << "NO SONAR OF ENEMY SHIP"<<std::endl<<std::endl;
       board_values();
       //NULL;
     }
@@ -149,26 +176,17 @@ while (loop_controller){
 
   }//point else statement ends
 
-}//while loop end
+}//for loop end
 
-
-std::cout << "Heres a rating of your performance!!!"<<std::endl;
-if (points < 10){
-  std::cout << std::endl<<"Ehh"<<std::endl;
+if ((myboard[randy_1]=="S") && (myboard[randy_2])=="S") {
+  std::cout << "Congratulations on winning!!! You are truly a master of SONAR technology"<<std::endl;
+  std::cout << "You are now know as HERO "<<name<<std::endl;
+  std::cout << ":)"<<std::endl;
 }
-else if(points<15){
-  std::cout << std::endl<< "EHHHHHHHH"<<std::endl;
+else{
+  std::cout << "You failed to protect the people you were supposed to"<<std::endl;
+  std::cout << ":( (in case you cant tell thats sad face jo being sad)"<<std::endl;
 }
-else if(points < 20){
-  std::cout << std::endl<< "eh"<<std::endl;
-}
-else if(points < 22){
-  std::cout << std::endl<< "Good!"<<std::endl;
-}
-else if(points <= 27){
-  std::cout <<  std::endl<<"Awesome!"<<std::endl;
-}
-
 
 std::cout << "play again (Y or N)"<<std::endl;
 std::cin >> play_again;
@@ -188,3 +206,5 @@ return 0;
 // make it give points CHECK
 // make it check if locator is a int
 //check if it gives the same int CHECK
+// turn into battlship CHECK
+// make it losable CHECK
